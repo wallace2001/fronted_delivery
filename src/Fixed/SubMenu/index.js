ecf3c4f0b3d2ec955c11ae877d5afd89ec588756
wallace de paula <kollen22@outlook.com>
2021-02-08 22:35:35 -0300

    atualizando rota de cardapio

--- src/Fixed/SubMenu/index.js
+++ src/Fixed/SubMenu/index.js
@@ -23,7 +23,7 @@ export const SubMenu = ({ item, index }) => {
         <SidebarLink to={item.path} onClick={nextPage} id={item.path}>
             <SidebarLink.first>
                 {item.icon}
-                <SidebarLink.Label>{item.title}</SidebarLink.Label>
+                <SidebarLink.Label onClick={nextPage} id={item.path}>{item.title}</SidebarLink.Label>
             </SidebarLink.first>
         </SidebarLink>
         {subnav && item.subNav.map((item, index) => {
@@ -31,7 +31,7 @@ export const SubMenu = ({ item, index }) => {
                 <SidebarLink.DropDownLink to={item.path} id={item.path} key={index} onClick={nextPage}>
                     <SidebarLink.first>
                         {item.icon}
-                        <SidebarLink.Label1>{item.title}</SidebarLink.Label1>
+                        <SidebarLink.Label1 onClick={nextPage} id={item.path}>{item.title}</SidebarLink.Label1>
                     </SidebarLink.first>
                 </SidebarLink.DropDownLink>
             );
